b23a1619f2390b1e8bad69e7e9bb49b9c70dca95
Vaneez04 <vaneezsaraswat4@gmail.com>
2025-11-22 17:03:54 +0530

    second

--- BACKEND/config/mongodb.js
+++ BACKEND/config/mongodb.js
@@ -40,6 +40,6 @@
 import mongoose from "mongoose";
 const connectDB=async ()=>{
     mongoose.connection.on('connected',()=> console.log("database connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/HCL_HACKATHON_DB`)
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
 }
 export default connectDB
